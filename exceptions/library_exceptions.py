b5db1c8fdd10b4cc45454562594ea36a55f33d9c
Renelyn Dino <maycdino24@gmail.com>
2026-05-21 00:08:21 +0800

    Add custom exceptions for library errors
    Added custom exception classes for library management.

--- exceptions/library_exceptions.py
+++ exceptions/library_exceptions.py
@@ -1,1 +1,17 @@
+# Exception classes
+# Used for handling system errors
 
+class BookNotFoundError(Exception):
+    pass
+
+class MemberNotFoundError(Exception):
+    pass
+
+class BookUnavailableError(Exception):
+    pass
+
+class LoanNotFoundError(Exception):
+    pass
+
+class BookAlreadyReturnedError(Exception):
+    pass
