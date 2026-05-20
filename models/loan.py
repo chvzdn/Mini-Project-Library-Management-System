bbc9d60ee797f8c85a8c24fd8c8033dd56dc22bd
Renelyn Dino <maycdino24@gmail.com>
2026-05-21 00:09:26 +0800

    Update loan.py

--- models/loan.py
+++ models/loan.py
@@ -1,1 +1,13 @@
+# Loan class
+# Represents a borrowed book transaction
 
+class Loan:
+    def __init__(self, loan_id, book, member):
+        self.loan_id = loan_id
+        self.book = book
+        self.member = member
+        self.is_active = True
+
+    def return_book(self):
+        self.is_active = False
+        self.book.available = True
