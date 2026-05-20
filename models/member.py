9ceb8dcb5179acda0f44e8f7e873a2cd3a525977
Renelyn Dino <maycdino24@gmail.com>
2026-05-21 00:09:42 +0800

    Update member.py

--- models/member.py
+++ models/member.py
@@ -1,1 +1,8 @@
+# Member class
+# Represents a library member
 
+class Member:
+    def __init__(self, member_id, name, email):
+        self.member_id = member_id
+        self.name = name
+        self.email = email
