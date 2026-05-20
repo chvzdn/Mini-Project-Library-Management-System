fe9cdb82ac27dbc468ecd427e8571e9270b4eb65
Renelyn Dino <maycdino24@gmail.com>
2026-05-21 00:09:09 +0800

    Update book.py

--- models/book.py
+++ models/book.py
@@ -1,1 +1,9 @@
+# Book class
+# This class represents a library book
 
+class Book:
+    def __init__(self, book_id, title, author):
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.available = True
